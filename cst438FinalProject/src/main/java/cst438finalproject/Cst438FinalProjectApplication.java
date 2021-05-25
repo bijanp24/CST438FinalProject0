7bdd08225eb3b0e266925de61f5e572591cd374e
bijanlee <57022377+bijanlee@users.noreply.github.com>
2021-05-24 19:50:13 -0700

    First Commit

---
+++ cst438FinalProject/src/main/java/cst438finalproject/Cst438FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package cst438finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438FinalProjectApplication.class, args);
+	}
+
+}
---
+++ cst438FinalProject/src/test/java/cst438finalproject/Cst438FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cst438finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
